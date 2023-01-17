53f429e5bc0f812b384fd6a333f79394b524f196
TSanni <tomas.sanni@gmail.com>
2023-01-16 19:16:29 -0600

    Initial Commit

---
+++ MercariClone/MercariCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercariCloneApp.swift
+//  MercariClone
+//
+//  Created by Tomas Sanni on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MercariCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
